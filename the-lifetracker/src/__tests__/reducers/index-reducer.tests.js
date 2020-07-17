a48627528f0f50524b0bf24581756aed5326cb89
brevin <cronkbrevin98@gmail.com>
2020-07-17 16:53:32 -0700

    bug with trying to locate firestore in my tests, debug

---
+++ the-lifetracker/src/__tests__/reducers/index-reducer.tests.js
@@ -0,0 +1,14 @@
+import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+
+let store = createStore(rootReducer);
+
+describe('rootReducer', () => {
+	// Test One
+	test('Should return default state if no action type is recognized', () => {
+		expect(rootReducer({}, { type: null })).toEqual({
+			masterItemList: {},
+			formVisibleOnPage: false
+		});
+	});
+});
