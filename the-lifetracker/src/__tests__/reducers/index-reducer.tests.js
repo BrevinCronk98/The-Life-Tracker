476de8837854bd372e5c4a5d667307e5ef665d51
brevin <cronkbrevin98@gmail.com>
2020-07-17 17:30:06 -0700

    I dont think I can correctly test my root reducer, the value that the firestorer reducer returns is something I write in a test

--- the-lifetracker/src/__tests__/reducers/index-reducer.tests.js
+++ the-lifetracker/src/__tests__/reducers/index-reducer.tests.js
@@ -1,14 +1,15 @@
 import rootReducer from '../../reducers/index';
 import { createStore } from 'redux';
+import { firestoreReducer } from 'react-redux-firebase';
 
 let store = createStore(rootReducer);
-
 describe('rootReducer', () => {
 	// Test One
 	test('Should return default state if no action type is recognized', () => {
 		expect(rootReducer({}, { type: null })).toEqual({
 			masterItemList: {},
-			formVisibleOnPage: false
+			formVisibleOnPage: false,
+			firestore: any
 		});
 	});
 });
