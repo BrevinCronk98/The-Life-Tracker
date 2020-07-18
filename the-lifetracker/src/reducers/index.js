a9a91abd04bdd3485d38e475401013a1bb6b328a
brevin <cronkbrevin98@gmail.com>
2020-07-17 17:17:43 -0700

    found the bug, test still failed, debug

--- the-lifetracker/src/reducers/index.js
+++ the-lifetracker/src/reducers/index.js
@@ -1,7 +1,7 @@
 import formVisibleReducer from './form-visible-reducer';
 import itemListReducer from './item-list-reducer';
 import { combineReducers } from 'redux';
-import { firestoreReducer } from 'react-firestore';
+import { firestoreReducer } from 'redux-firestore';
 
 const rootReducer = combineReducers({
 	formVisibleOnPage: formVisibleReducer,
