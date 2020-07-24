3b2bb683aa6298b075bfac2ae8c139c8c3bf0ad2
brevin <cronkbrevin98@gmail.com>
2020-07-24 10:15:16 -0700

    Futher work on the rendering ToDoList components then work on stylizing

--- the-lifetracker/src/components/Header.js
+++ the-lifetracker/src/components/Header.js
@@ -7,6 +7,7 @@ function Header() {
 				<h1>Welcome to Your To Do List!</h1>
 				<h3 id="toDoGreet">Feel free to add to your to do list at any time!</h3>
 				<h3 id="toDoGreet">We want to help you do what you need to do!</h3>
+				<h3>Thank You Forr Using Life Tracker</h3>
 			</div>
 			<hr />
 		</React.Fragment>
