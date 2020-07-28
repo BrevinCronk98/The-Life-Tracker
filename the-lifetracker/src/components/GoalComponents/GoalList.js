6a056721d4f372facd369cd0d00f7b58b1ebb644
brevin <cronkbrevin98@gmail.com>
2020-07-28 13:42:49 -0700

    Work on stylizing and page layout

--- the-lifetracker/src/components/GoalComponents/GoalList.js
+++ the-lifetracker/src/components/GoalComponents/GoalList.js
@@ -5,6 +5,7 @@ import { useSelector } from 'react-redux';
 import { useFirestoreConnect, isLoaded, useFirestore } from 'react-redux-firebase';
 import 'fontsource-roboto';
 import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
 function GoalList(props) {
 	useFirestoreConnect([ { collection: 'goals' } ]);
@@ -14,6 +15,17 @@ function GoalList(props) {
 	if (isLoaded(goals)) {
 		return (
 			<React.Fragment>
+				<center>
+					<Container maxWidth="sm" component="main">
+						<Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+							Your Goals!
+						</Typography>
+						<Typography variant="h5" align="center" color="textSecondary" component="p">
+							This is the place to keep track of daily goals! Remember roadblocks don't mean failure, keep
+							pursuing. Make your goals meaningful to you!
+						</Typography>
+					</Container>
+				</center>
 				<hr />
 				{goals.map((goal) => {
 					return <Goal goalName={goal.goalName} goalNotes={goal.goalLocation} id={goal.id} />;
--- the-lifetracker/src/components/Header.js
+++ the-lifetracker/src/components/Header.js
@@ -6,29 +6,64 @@ import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core';
 import rootReducer from '../reducers/index';
 import { Link } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+	mainGrid: {
+		margin: theme.spacing(3, 50, 3)
+	}
+}));
 
 function Header() {
 	let HeaderText = null;
+	const classes = useStyles();
 	return (
 		<React.Fragment>
-			<Toolbar>
+			<center>
 				<Typography variant="h6" color="inherit" noWrap>
 					The Life Tracker
 				</Typography>
-				<Button id="signin" href="#" color="primary" variant="outlined">
-					<Link style={{ textDecoration: 'none' }} to="/ToDo">
-						To Do List
-					</Link>
-				</Button>
-				<Button id="signin" href="#" color="primary" variant="outlined">
-					Login
-				</Button>
-				<Button id="signin" href="#" color="primary" variant="outlined">
-					<Link style={{ textDecoration: 'none' }} to="/Goals">
-						Goals
-					</Link>
-				</Button>
+			</center>
+			<br />
+			<Toolbar>
+				<Container>
+					<Grid item xs={1}>
+						<center>
+							<Button
+								className={classes.mainGrid}
+								id="todolist"
+								href="#"
+								color="primary"
+								variant="outlined"
+							>
+								<Link style={{ textDecoration: 'none' }} to="/ToDo">
+									To Do List
+								</Link>
+							</Button>
+						</center>
+					</Grid>
+					<Grid item xs={1}>
+						<Button className={classes.mainGrid} id="signin" href="#" color="primary" variant="outlined">
+							Login
+						</Button>
+					</Grid>
+					<Grid item xs={1}>
+						<Button
+							className={classes.mainGrid}
+							align="center"
+							id="signin"
+							href="#"
+							color="primary"
+							variant="outlined"
+						>
+							<Link style={{ textDecoration: 'none' }} to="/Goals">
+								Goals
+							</Link>
+						</Button>
+					</Grid>
+				</Container>
 			</Toolbar>
+			<hr />
 		</React.Fragment>
 	);
 }
