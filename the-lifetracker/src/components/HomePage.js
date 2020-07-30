861ce5c2a9d1f16929c187088cf458978c112271
brevin <cronkbrevin98@gmail.com>
2020-07-30 09:19:29 -0700

    Added clickable pictures to the homepage to reroute users

--- the-lifetracker/src/components/HomePage.js
+++ the-lifetracker/src/components/HomePage.js
@@ -6,36 +6,20 @@ import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import { Typography } from '@material-ui/core';
 import { Link } from 'react-router-dom';
-import PostImgOne from '../img/PostImg1.webp';
-import PostImgTwo from '../img/PostImgOne.jpg';
-import PostImgThree from '../img/PostImgEight.jpg';
-import PostImgFour from '../img/PostImgEleven.jpg';
-import PostImgFive from '../img/PostImgFifteen.jpg';
-import PostImgSix from '../img/PostImgFive.jpg';
-import PostImgSeven from '../img/PostImgFour.jpg';
-import PostImgEight from '../img/PostImgFourteen.jpg';
-import PostImgNine from '../img/PostImgNine.jpg';
-import PostImgTen from '../img/PostImgSeven.jpg';
-import PostImgEleven from '../img/PostImgSix.jpg';
-import PostImgTwelve from '../img/PostImgTen.jpg';
-import PostImgThirteen from '../img/PostImgThirteen.jpg';
-import PostImgFourteen from '../img/PostImgTwelve.webp';
-import Hidden from '@material-ui/core/Hidden';
+import ToDo from './../img/ToDoList.jpg';
+import Goals from './../img/Goals.jpg';
+import SignUp from './../img/SignUp.png';
 
-const imgArrayOne = [ PostImgOne, PostImgTwo, PostImgThree, PostImgFour ];
-const imgArrayTwo = [ PostImgNine, PostImgTen, PostImgEleven ];
-const imgArrayThree = [ PostImgFive, PostImgSix, PostImgSeven, PostImgEight ];
-
-let numOne = Math.floor(Math.random() * imgArrayOne.length);
-let numTwo = Math.floor(Math.random() * imgArrayTwo.length);
-let numThree = Math.floor(Math.random() * imgArrayThree.length);
-
-let imgOne = imgArrayOne[numOne];
-let imgTwo = imgArrayTwo[numTwo];
-let imgThree = imgArrayThree[numThree];
-
-function yupp() {
+function toDoSwitch() {
 	window.open('/ToDo');
+}
+
+function goalSwitch() {
+	window.open('/Goals');
+}
+
+function signUpSwitch() {
+	window.open('/SignIn');
 }
 
 function HomePage() {
@@ -54,23 +38,34 @@ function HomePage() {
 			</Container>
 			<center>
 				<Grid item xs={12} md={3}>
-					<Button style={{ marginBottom: '8%' }} id="main-btn" variant="contained" color="primary">
-						<img id="img" src={imgOne} width="45%" style={{ borderRadius: '15px' }} />
-						Sign Up
-					</Button>
-					<Button id="pic" style={{ marginBottom: '8%' }} id="main-btn" variant="contained" color="primary">
-						<img id="img" src={imgTwo} width="45%" style={{ borderRadius: '15px' }} />
-						To Do
-					</Button>
+					<input
+						onClick={toDoSwitch}
+						type="image"
+						width="45%"
+						src={ToDo}
+						style={{ marginRight: '4%', borderRadius: '15px' }}
+						class="btTxt submit"
+					/>
+					<input
+						style={{ borderRadius: '15px' }}
+						onClick={goalSwitch}
+						type="image"
+						src={Goals}
+						width="48%"
+						class="btTxt submit"
+					/>
 				</Grid>
 				<br />
-				<Grid style={{ backgroundImage: imgThree }} item xs={12} md={3}>
-					<img id="img" src={imgThree} width="45%" style={{ borderRadius: '15px' }} />
-					<Button style={{ marginBottom: '8%' }} id="main-btn" variant="contained" color="primary">
-						Goals
-					</Button>
+				<Grid item xs={12} md={3}>
+					<input
+						style={{ borderRadius: '15px' }}
+						onClick={signUpSwitch}
+						type="image"
+						src={SignUp}
+						width="45%"
+						class="btTxt submit"
+					/>
 				</Grid>
-				<input onClick={yupp} type="image" src={imgThree} class="btTxt submit" />
 			</center>
 		</React.Fragment>
 	);
